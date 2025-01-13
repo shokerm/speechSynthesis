82f5ca290a6583d910c57f96a90c7b1ca58a4048
shokerm <moshe@shoker.co.il>
2025-01-13 11:06:03 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const synth = window.speechSynthesis;
 const inputForm = document.querySelector("form");
 const inputTxt = document.querySelector("input");
 const voiceSelect = document.querySelector("select");
+//
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
+inputTxt.value = urlParams.get("q");
 let voices;
 
 function loadVoices() {
@@ -30,3 +34,19 @@ inputForm.onsubmit = (event) => {
   utterThis.voice = voices[voiceSelect.value];
   synth.speak(utterThis);
 };
+
+inputTxt.addEventListener("change", (e) => {
+  updateURLParam("q", e.target.value);
+  console.log(e.target.value);
+});
+console.log(urlParams.get("q"));
+
+function updateURLParam(key, value) {
+  const url = new URL(window.location);
+  if (value) {
+    url.searchParams.set(key, value); // מוסיף או מעדכן פרמטר
+  } else {
+    url.searchParams.delete(key); // מסיר פרמטר אם השדה ריק
+  }
+  window.history.replaceState({}, "", url); // מעדכן את ה-URL ללא רענון הדף
+}
